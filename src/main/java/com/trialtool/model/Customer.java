322c8626eb1a0a5cb210fd061128c412b5a38c93
TimSelivan <timselivanson@gmail.com>
2023-03-27 17:14:23 +0300

    Implement model layer

--- src/main/java/com/trialtool/model/Customer.java
+++ src/main/java/com/trialtool/model/Customer.java
@@ -1,4 +1,55 @@
 package com.trialtool.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Size(min = 2, max = 100, message = "Name should be not less then 2 and more then 100 symbols")
+    @Column(name = "name")
+    private String name;
+    @Column(name = "phone")
+    private int phone;
+    @OneToMany(mappedBy = "customer")
+    List<Tool> tools;
+
+    public Customer() {
+
+    }
+
+    public Customer(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public List<Tool> getTools() {
+        return tools;
+    }
+
+    public void setTools(List<Tool> tools) {
+        this.tools = tools;
+    }
 }
--- src/main/java/com/trialtool/model/Tool.java
+++ src/main/java/com/trialtool/model/Tool.java
@@ -1,4 +1,85 @@
 package com.trialtool.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
+@Table(name = "tool")
 public class Tool {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty(message = "Please enter the brand")
+    @Column(name = "brand")
+    private String brand;
+    @NotEmpty(message = "Please enter the type")
+    @Column(name = "type")
+    private String type;
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+    @Column(name = "taken_on")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date takenOn;
+    @Transient
+    private boolean expired; //Hibernate shouldn`t see this field
+
+    public Tool() {
+    }
+
+    public Tool(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Date getTakenOn() {
+        return takenOn;
+    }
+
+    public void setTakenOn(Date takenOn) {
+        this.takenOn = takenOn;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
 }
