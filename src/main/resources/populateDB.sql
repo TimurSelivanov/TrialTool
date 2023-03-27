14673a214e6f6392391bcc6ed4b702dd20f2a911
TimSelivan <timselivanson@gmail.com>
2023-03-27 12:08:50 +0300

    Add database and SQL scripts for datatables initialization and population

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,9 @@
+insert into customer(name, phone)
+values ('Tom', 123),
+       ('Ann', 456),
+       ('Bob', 789);
+
+insert into tool(brand, type)
+values ('Bosch', 'Table saw'),
+       ('Makita', 'Chain saw'),
+       ('Dewalt', 'Miter saw');
