60c51ba76c976b015fe588dc37de4a4cccb489dc
TimSelivan <timselivanson@gmail.com>
2023-03-28 11:17:29 +0300

    Implement repository layer

---
+++ src/main/java/com/trialtool/repository/CustomersRepository.java
@@ -0,0 +1,12 @@
+package com.trialtool.repository;
+
+import com.trialtool.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByName(String name);
+}
---
+++ src/main/java/com/trialtool/repository/ToolsRepository.java
@@ -0,0 +1,13 @@
+package com.trialtool.repository;
+
+import com.trialtool.model.Tool;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ToolsRepository extends JpaRepository<Tool, Integer> {
+    List<Tool> findByTitleStartingWith(String title);
+}
+
